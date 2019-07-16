ec90c3a7912bb55874111a42e4a40e17d71250c8
Joshim Uddin <joshimcse@gmail.com>
2019-07-16 14:41:03 +0600

    update validator

--- server/validator/validator.js
+++ server/validator/validator.js
@@ -6,57 +6,44 @@ module.exports = {
   validateRegisterInput: data => {
     let errors = {};
 
-    for(key in data) {
+    for (let key in data) {
       data[key] = !isEmpty(data[key]) ? data[key].trim() : '';
-    }
-
-    if (!validator.isLength(data.firstName, { min: 2, max: 20 })) {
-      errors.firstName = 'First Name must be between 2 and 20 characters.';
     }
 
     if (isEmpty(data.firstName)) {
       errors.firstName = 'First Name field is required';
-    }
-
-    if (!validator.isLength(data.lastName, { min: 2, max: 20 })) {
-      errors.lastName = 'Last Name must be between 2 and 30 characters.';
+    } else if (!validator.isLength(data.firstName, { min: 2, max: 20 })) {
+      errors.firstName = 'First Name must be between 2 and 20 characters.';
     }
 
     if (isEmpty(data.lastName)) {
       errors.lastName = 'Last Name field is required';
-    }
-
-    if (!validator.isEmail(data.email)) {
-      errors.email = 'Email is invalid';
+    } else if (!validator.isLength(data.lastName, { min: 2, max: 20 })) {
+      errors.lastName = 'Last Name must be between 2 and 30 characters.';
     }
 
     if (isEmpty(data.email)) {
       errors.email = 'Email field is required';
-    }
-
-    if (!isBdNumber(data.phone)) {
-      errors.phone = 'Phone Number is invalid';
+    } else if (!validator.isEmail(data.email)) {
+      errors.email = 'Email is invalid';
     }
 
     if (isEmpty(data.phone)) {
       errors.phone = 'Phone Number field is required';
+    } else if (!isBdNumber(data.phone)) {
+      errors.phone = 'Phone Number is invalid';
     }
 
-    
-    if (!validator.equals(data.password, data.confirmPassword)) {
-      errors.password = 'Password must match.';
-    }
-    
-    if (!validator.isLength(data.password, { min: 6, max: 30 })) {
-      errors.password = 'Password must be at least 6 characters';
-    }
-    
     if (isEmpty(data.password)) {
       errors.password = 'Password field is required';
+    } else if (!validator.isLength(data.password, { min: 6, max: 30 })) {
+      errors.password = 'Password must be at least 6 characters';
     }
 
     if (isEmpty(data.confirmPassword)) {
       errors.confirmPassword = 'Confirm Password field is required';
+    } else if (!validator.equals(data.password, data.confirmPassword)) {
+      errors.confirmPassword = 'Password must match.';
     }
 
     const validationData = {
@@ -72,20 +59,17 @@ module.exports = {
   validateLoginInput: data => {
     let errors = {};
 
-
-    for(key in data) {
+    for (let key in data) {
       data[key] = !isEmpty(data[key]) ? data[key].trim() : '';
-    }
-
-    if (
-      !validator.isEmail(data.emailOrPhone) &&
-      !isBdNumber(data.emailOrPhone)
-    ) {
-      errors.emailOrPhone = 'Email or Phone Number is not valid';
     }
 
     if (isEmpty(data.emailOrPhone)) {
       errors.emailOrPhone = 'Email or Phone Number must be required';
+    } else if (
+      !validator.isEmail(data.emailOrPhone) &&
+      !isBdNumber(data.emailOrPhne)
+    ) {
+      errors.emailOrPhone = 'Email or Phone Number is not valid';
     }
 
     if (isEmpty(data.password)) {
